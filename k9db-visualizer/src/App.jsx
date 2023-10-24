1e9dd188d2068b529ea73999ddd318e5088c561a
Nora Tang <jingyu_tang@brown.edu>
2023-10-24 16:13:10 -0400

    add temp global const

--- k9db-visualizer/src/App.jsx
+++ k9db-visualizer/src/App.jsx
@@ -3,7 +3,51 @@ import "./App.css";
 
 import Flow from "./components/FlowComponent/Flow";
 import MyCollapse from "./components/MenuComponent/Collapse";
+
 export default function App() {
+
+  const parserRes = [[ { annotation: 'data_subject', tableName: 'users' } ],
+  [
+    {
+      annotation: 'owned_by',
+      from: 'stories',
+      to: 'user',
+      edgeName: 'author'
+    }
+  ],
+  [],
+  [
+    {
+      annotation: 'owned_by',
+      from: 'taggings',
+      to: 'stories',
+      edgeName: 'story_id'
+    },
+    {
+      annotation: 'accesses',
+      from: 'tag',
+      to: 'taggings',
+      edgeName: 'tag_id'
+    }
+  ],
+  [
+    {
+      annotation: 'owned_by',
+      from: 'messages',
+      to: 'user',
+      edgeName: 'sender'
+    },
+    {
+      annotation: 'owned_by',
+      from: 'messages',
+      to: 'user',
+      edgeName: 'receiver'
+    }
+  ]]
+
+  const dataSubject = ['users'];
+  const otherTables = ['messages', 'taggings', 'tags', 'stories'];
+
   return (
     <div>
       <div class="split left">
