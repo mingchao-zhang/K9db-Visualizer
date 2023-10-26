1cd93f9bc98b973904acf95b04cf0c747fa7cef2
Nora Tang <jingyu_tang@brown.edu>
2023-10-26 12:55:26 -0400

    fix newline issue

--- k9db-visualizer/parser/parse.js
+++ k9db-visualizer/parser/parse.js
@@ -101,47 +101,50 @@ function parseCreateStatement(inputString) {
     return res
   }
   
-const parse = function (statement){
+const parseHelper = function (statement) {
     if (statement.toLowerCase().indexOf("create") !== -1) {
         return parseCreateStatement(statement)
     }
-    return "not valid"
+    return []
+}
+
+const parse = function (input){
+    let createStatements = input.split(/;\s+/).map(statement => statement.replace(/\\n/g, ''));
+    console.log(createStatements);
+    let res = []
+    for (const statement of createStatements) {
+        let parsedRes = parseHelper(statement)
+        if (parsedRes.length !== 0){
+            res.push(parsedRes)
+        }
+    }
+    return res
 }
 
 export default parse;
 
 //-----------------------------------------------------------------------------
 // TESTS
-const createStatements = [
-    `CREATE DATA_SUBJECT TABLE users (
-        id INT PRIMARY KEY
-    );`,
-    `CREATE TABLE stories (
-        id INT PRIMARY KEY,
-        title TEXT,
-        author INT NOT NULL OWNED_BY user(id) 
-    );`,
-    `CREATE TABLE tags (
-        id INT PRIMARY KEY,
-        tag TEXT
-    );`,
-    `CREATE TABLE taggings (
-        id INT PRIMARY KEY,
-        story_id INT NOT NULL OWNED_BY stories(id), 
-        tag_id INT NOT NULL ACCESSES tag(id)
-    );`,
-    `CREATE TABLE messages (
-        id INT PRIMARY KEY, 
-        body text, 
-        sender INT NOT NULL OWNED_BY user(id), 
-        receiver INT NOT NULL OWNED_BY user(id), 
-        ON DEL sender ANON (sender),
-        ON DEL receiver ANON (receiver)
-    );`
-]
-
-// for (const statement of createStatements) {
-//     let parsedRes = parse(statement)
-//     console.log(parsedRes)
-// }
-  
+const input = `CREATE DATA_SUBJECT TABLE users (
+    id INT PRIMARY KEY
+); CREATE TABLE stories (
+    id INT PRIMARY KEY,
+    title TEXT,
+    author INT NOT NULL OWNED_BY user(id) 
+); CREATE TABLE tags (
+    id INT PRIMARY KEY,
+    tag TEXT
+); CREATE TABLE taggings (
+    id INT PRIMARY KEY,
+    story_id INT NOT NULL OWNED_BY stories(id), 
+    tag_id INT NOT NULL ACCESSES tag(id)
+);CREATE TABLE messages (
+    id INT PRIMARY KEY, 
+    body text, 
+    sender INT NOT NULL OWNED_BY user(id), 
+    receiver INT NOT NULL OWNED_BY user(id), 
+    ON DEL sender ANON (sender),
+    ON DEL receiver ANON (receiver)
+);
+`  
+console.log(parse(input))
